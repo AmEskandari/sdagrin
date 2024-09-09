08af0c8e4f47bdc79c222d97f1b0ee4c3ce86055
Amir Eskandari <amireskandari1998@gmail.com>
2024-09-09 15:23:48 -0400

    Add files via upload

---
+++ scripts/saved_models/bay_point/dasd.py
@@ -0,0 +1,25 @@
+
+python ./scripts/run_imputation.py --config config/grin/air.yaml --in-sample False --checkpoint-dir scripts/logs/air/grin/2024-02-15_16-35-47_940200406/epoch=23-step=3839.ckpt --evaluation True 
+
+
+
+python ./scripts/run_imputation.py --config config/grin/air36.yaml --in-sample False --checkpoint-dir scripts/logs/air36/grin/2024-07-26_20-26-53_161862516/epoch=5-step=959.ckpt --evaluation True 
+
+
+
+/Users/amir/working_dir/sda-grin/scripts/logs/air36/grin/2024-07-26_20-26-53_161862516
+
+
+
+python ./scripts/run_imputation.py --config config/grin/la_point.yaml --checkpoint-dir scripts/logs/la_point/grin/2024-02-27_05-47-46_634632353/epoch=70-step=11359.ckpt --evaluation True 
+
+
+
+python ./scripts/run_imputation.py --config config/grin/bay_point.yaml --checkpoint-dir scripts/logs/la_point/grin/2024-02-17_01-03-53_609509716/epoch=49-step=7999.ckpt --evaluation True 
+
+
+
+
+/Users/amir/working_dir/sda-grin/scripts/logs/bay_point/grin/2024-02-17_01-03-53_609509716
+
+/Users/amir/working_dir/sda-grin/scripts/logs/la_point/grin/2024-02-27_05-47-46_634632353
